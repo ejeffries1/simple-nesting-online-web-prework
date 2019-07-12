e8a7cf37e7c7ff4d8b19eee2bdcd4d9f60681dc0
Evan Jeffries <ejeffries14@gmail.com>
2019-07-12 03:17:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,6 @@ def adding_matz
 #:known_for => "Ruby",
     #:languages => ["LISP", "C"]
     
-programmer_hash = [:yukihiro_matsumoto] << {:known_for => "Ruby", :languages => ["LISP", "C"]
 }
  
 	programmer_hash = 
@@ -83,7 +82,8 @@ programmer_hash = [:yukihiro_matsumoto] << {:known_for => "Ruby", :languages => 
           :languages => ["C"]
         }
      }
-     
+     programmer_hash = [:yukihiro_matsumoto] << {:known_for => "Ruby", :languages => ["LISP", "C"]
+}
 programmer_hash[:yukihiro_matsumoto]
 end
 
