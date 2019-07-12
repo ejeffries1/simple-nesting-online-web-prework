cebc7e1f982155d347c94d2e9e1474dad7866a05
Evan Jeffries <ejeffries14@gmail.com>
2019-07-12 03:17:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,8 +63,6 @@ def adding_matz
 # add the following information to the top level of
 #:known_for => "Ruby",
     #:languages => ["LISP", "C"]
-    
-}
  
 	programmer_hash = 
  		{
@@ -82,8 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash = [:yukihiro_matsumoto] << {:known_for => "Ruby", :languages => ["LISP", "C"]
-}
+    
 programmer_hash[:yukihiro_matsumoto]
 end
 
